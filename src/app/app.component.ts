d23deb0ac88cb8882f2ceb41828b948da92848c7
PascuDenis <37848035+PascuDenis@users.noreply.github.com>
2019-05-27 15:45:28 +0300

    Some angular example project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'some-angular-example';
+  title = 'Tour of Heroes';
 }
+
+// the component class code, written in TypeScript
